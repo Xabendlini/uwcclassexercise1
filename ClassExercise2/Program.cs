51a94cf8f8fc9ff9591d1128f485e457a8120cb7
Xabendlini <151826333+Xabendlini@users.noreply.github.com>
2023-11-28 11:01:34 +0200

    First Commit

---
+++ ClassExercise2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] AllNumbers = { -1, 2, 4, 6, 9 };
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
